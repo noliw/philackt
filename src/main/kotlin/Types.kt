a3dc24063668d37e4a5a68cfade691177824befd
noliw <noliw2017@gmail.com>
2023-08-10 01:02:08 -0700

    Reorg

--- src/main/kotlin/Types.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    var name = "Nolawi"
-    var age = 22
-    var favNum = 10
-    println("Hello everybody!!! My name is $name and I am $age years old. You would be surprised to find out that my favorite number is $favNum")
-}
