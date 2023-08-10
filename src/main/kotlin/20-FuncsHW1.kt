f98cad68b921acb26abaa9df99851d26a2f933c1
noliw <noliw2017@gmail.com>
2023-08-10 01:29:49 -0700

    Funcs with return hw

--- src/main/kotlin/20-FuncsHW1.kt
+++ src/main/kotlin/20-FuncsHW1.kt
@@ -1,3 +1,20 @@
 fun main() {
+    findNum(listOf(3,2,5,1,5,6,7))
+}
 
+fun findNum(list: List<Int>) { // [3,2,5,6,7]
+    println("The list looks like $list")
+    println("Search for this number:")
+    val num = readLine()?.toInt()
+
+    if (num != null) {
+        val index = list.indexOf(num)
+        if (index != -1) {
+            println("The index of $num is $index")
+        } else {
+            println("The number you entered is not in the list")
+        }
+    } else {
+        println("Invalid input")
+    }
 }
