279f79743adb224372975c6240d541984f9c0c98
noliw <noliw2017@gmail.com>
2023-08-11 01:52:23 -0700

    Classses 1

---
+++ src/main/kotlin/25-Classes.kt
@@ -0,0 +1,10 @@
+//  a class is a blueprint for an object
+
+
+fun main() {
+    val myRect = Rectangle(4.0, 7.0)
+    println(" Rectangle area is ${myRect.area()}")
+    println(" Rectangle Perimeter is ${myRect.perimeter()}")
+    println(" Rectangle  is square?  ${myRect.isSquare()}")
+}
+
---
+++ src/main/kotlin/26-RectangleCLass.kt
@@ -0,0 +1,15 @@
+class Rectangle( // constructor is used to specify the values we need to create a class
+    val a: Double,
+    val b: Double
+) {
+    init {
+        println("Rectangle crete with a = $a and b = $b")
+    }
+
+    fun area() = a * b
+
+    fun perimeter() = 2 * a + 2 * b
+
+    fun isSquare() = a == b
+
+}
